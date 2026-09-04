2a0b5eba04402c72d0ebd4d29eb30d3bd00dff72
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-09-04 12:25:17 -0400

    chore: remove unused address helper
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- src/lib/dingo/address.ts
+++
@@ -1,7 +0,0 @@
-import { Core } from "@blaze-cardano/sdk";
-
-// CIP-30 reports addresses as hex-encoded CBOR bytes; Blockfrost (and humans)
-// expect bech32. This is the one place that conversion happens.
-export function hexAddressToBech32(hex: string): string {
-  return Core.Address.fromBytes(Core.HexBlob(hex)).toBech32();
-}
